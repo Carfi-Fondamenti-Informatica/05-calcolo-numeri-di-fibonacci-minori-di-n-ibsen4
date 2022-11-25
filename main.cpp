0a00eca9f4664ee1ec8e472286f1e490cedf22e1
ibsen4 <115497034+ibsen4@users.noreply.github.com>
2022-11-25 15:53:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main(){
+    int n=0, a=1, b=1,c=0;
+    cin>>n;
+        cout << a << endl << b << endl;
+        for(;;) {
+            c = a + b;
+            a = b;
+            b = c;
+            if (c<n) {
+                cout << c << endl;
+            }
+            else {
+                break;
+            }
+        }
+    return 0;
 }
