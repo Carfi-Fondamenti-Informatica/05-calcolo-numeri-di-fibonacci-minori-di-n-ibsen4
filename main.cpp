5f9a7510309a9e955d34ae47be02e104e44ca148
ibsen4 <115497034+ibsen4@users.noreply.github.com>
2022-11-27 18:57:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,17 @@ using namespace std;
 int main(){
     int n=0, a=1, b=1,c=0;
     cin>>n;
-    cout << a << endl << b << endl;
-        for(int  i=0;i<=n;i++) {
+        cout << a << endl << b << endl;
+        for(int i=0;i<=n  ;i++) {
             c = a + b;
             a = b;
             b = c;
-        cout << c << endl;
-                }
-    return 0;
-}
+            if (c<n) {
+                cout << c << endl;
+            }
+            else {
+                break;
+            }
+        }
+    return 0
+    }
